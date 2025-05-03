37ee1c6cc5cbc2bb5622ea6fde3e74ae1d3a492d
Servilleta36 <alebagon05@gmail.com>
2025-05-03 21:04:21 +0200

    Limite hecho y revisar bidireccional e iterativa

--- modulos/B_Prof_Bid.py
+++ modulos/B_Prof_Bid.py
@@ -6,7 +6,10 @@ class Bidireccional:
         self.posS_x = 0
         self.posS_y = 0
         self.padres = [[None for _ in range(len(self.lab.tab[0]))] for _ in range(len(self.lab.tab))]
+        self.padres2 = [[None for _ in range(len(self.lab.tab[0]))] for _ in range(len(self.lab.tab))]
         self.visitados = [[0 for _ in range(len(self.lab.tab[0]))] for _ in range(len(self.lab.tab))]
+        self.visitados2 = [[0 for _ in range(len(self.lab.tab[0]))] for _ in range(len(self.lab.tab))]
+        self.punto_encuentro=None
 
         for i in range(len(self.lab.tab)):  # coordenadas de la entrada
             for j in range(len(self.lab.tab[i])):
@@ -34,39 +37,85 @@ class Bidireccional:
 
     def moverse(self):
         pila = [(self.posE_x, self.posE_y)]
-        self.visitados[self.posE_x][self.posE_y] = 1
+        pila2=[(self.posS_x, self.posS_y)]
+        self.visitados[self.posE_x][self.posE_y]=1
+        self.visitados2[self.posS_x][self.posS_y]=1
         iter = 0
-        while pila:
-            camino = False
+        encontrado=False
 
-            if pila[-1] == (
-            self.posS_x, self.posS_y):  # El -1 indica la ultima posición de la pila, es decir la posicion de la meta
-                print("Se ha encontrado la salida en " + str(iter) + " iteraciones")
-                self.puntos()
-                return
+        while pila and encontrado==False:
+            camino=False
+            camino2=False
+            p_actual=pila[-1]
+            p_actual2=pila2[-1]
 
-            p_actual = pila[-1]
-            validos = self.buscarVecino(p_actual[0], p_actual[1])
+            validos=self.buscarVecino(p_actual[0],p_actual[1])
+            validos2=self.buscarVecino(p_actual2[0],p_actual2[1])
 
             for i, j in validos:  # comprueba si hay caminos validos y los almacena, sino se quita de la pila
-                if self.visitados[i][j] == 0:
-                    self.visitados[i][j] = 1
-                    pila.append((i, j))
-                    self.padres[i][j] = p_actual
+                if self.visitados2[i][j]==1:
+                    encontrado=True
+                    self.punto_encuentro=(i, j)
+                    break
 
-                    camino = True
-                    iter = iter + 1
+                if self.visitados[i][j]==0:
+                    self.visitados[i][j]=1
+                    pila.append((i, j))
+                    self.padres[i][j]=p_actual
+
+                    camino=True
+                    iter=iter + 1
+
+            for i, j in validos2:  # comprueba si hay caminos validos y los almacena, sino se quita de la pila
+                if self.visitados[i][j]==1:
+                    encontrado=True
+                    self.punto_encuentro=(i, j)
+                    break
+
+                if self.visitados2[i][j]==0:
+                    self.visitados2[i][j]=1
+                    pila2.append((i, j))
+                    self.padres2[i][j]=p_actual2
+
+                    camino2=True
+                    iter=iter+1
 
             if not camino:
                 pila.pop()
-                iter = iter - 1
+                iter=iter-1
 
-        print("No se ha encontrado el camino")
-        self.puntos()
+            if not camino2:
+                pila2.pop()
+                iter=iter-1
+
+        if encontrado==True:
+            print("Se ha encontrado la salida en "+str(iter)+" iteraciones")
+            self.puntos()
 
     def puntos(self):
-        x, y = self.posS_x, self.posS_y
+        if self.punto_encuentro is None:
+            print("No se puede reconstruir el camino: no hay punto de encuentro.")
+            return
+
+        # Camino desde el punto de encuentro hasta la entrada (E)
+        x, y = self.punto_encuentro
         while (x, y) != (self.posE_x, self.posE_y):
+            if self.padres[x][y] is None:
+                break
             if self.lab.tab[x][y] not in ("E", "S"):
-                self.lab.tab[x][y] = "."
+                self.lab.tab[x][y] = "*"
             x, y = self.padres[x][y]
+
+        # Camino desde S hacia el punto de encuentro usando padres2 (¡comenzamos en S!)
+        x, y = self.posS_x, self.posS_y
+        while (x, y) != self.punto_encuentro:
+            if self.padres2[x][y] is None:
+                break
+            if self.lab.tab[x][y] not in ("E", "S"):
+                self.lab.tab[x][y] = "*"
+            x, y = self.padres2[x][y]
+
+
+
+
+
